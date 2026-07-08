176a35cf706f711745c5664963c596254b4f5927
Prudhvi <prudhvisai3772@gmail.com>
2026-07-08 11:42:50 +0530

    add persistence and streamlit

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,10 @@
 from langgraph.graph import StateGraph, START, END
-from typing import List, TypedDict, Annotated
-from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.graph.message import add_messages
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+from typing import TypedDict, Annotated
 
 load_dotenv()
 
@@ -12,27 +12,23 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 
 class chat_state(TypedDict):
-    messages: Annotated[List[BaseMessage], add_messages]
+    messages: Annotated[list[BaseMessage], add_messages]
 
 
 graph = StateGraph(chat_state)
 
 
-def chat_node(state: chat_state):
+def chat_node(state: chat_state) -> chat_state:
     messages = state["messages"]
     response = model.invoke(messages)
     return {"messages": [response]}
 
+
+checkpointer = InMemorySaver()
 
 graph.add_node("chat", chat_node)
 
 graph.add_edge(START, "chat")
 graph.add_edge("chat", END)
 
-chat_bot = graph.compile()
-
-initial_state = {"messages": [HumanMessage(content="what is the capital of india")]}
-
-final_state = chat_bot.invoke(initial_state)
-
-print(final_state["messages"][-1])
+chatbot = graph.compile(checkpointer=checkpointer)
